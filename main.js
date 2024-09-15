5afc7d0bd30f90ad5cc1bbc646b59daf05bf08d3
Muhammad Umair Khan <64465232+M-Umair4290@users.noreply.github.com>
2024-09-15 10:36:26 +0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,46 @@
+alert("Welcome to JavaScript");
+
+//Arithematic Expressions
+
+//Addition
+
+var num1 = 30;
+var num2 = 12;
+var result = num1 + num2;
+console.log(result);
+
+//Subtraction
+
+var a = 17;
+var b = 5;
+var c = a + b;
+console.log(c);
+
+//Multiplication
+
+var num1 = 5;
+var num2 = 6;
+var res = num1 + num2;
+console.log(res);
+
+//Division
+
+var x = 84;
+var y = 12;
+console.log(x / y);
+
+
+
+var userName = "Umair";
+alert("Your Username is " + userName);
+
+
+
+
+/* illegal variable names
+let 1stName = "Umair";
+var first-name = "Umair";
+let @age = 25;
+let if = 8;
+let user name = "Umair";
+*/
